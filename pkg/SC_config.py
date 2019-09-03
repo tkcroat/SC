712969f699d5e350ba446bb8dd2bbd52c907d908
Kevin Croat <kcroat@gmail.com>
2019-09-03 06:17:37 -0500

    config mods

--- pkg/SC_config.py
+++ pkg/SC_config.py
@@ -7,6 +7,6 @@ Created on Mon Aug 26 21:29:30 2019
 
 from os.path import expanduser
 homedir = expanduser("~")
-_OUTPUT_DIR=homedir+'\\'+"Sponsors_Club"
+_OUTPUT_DIR=homedir+"\\Documents\\Sponsors_Club"
 
-_INPUT_DIR=homedir+'\\'+"Sponsors_Club"
+_INPUT_DIR=homedir+"\\Documents\\Sponsors_Club\\SC_files"
